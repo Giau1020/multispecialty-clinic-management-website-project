c80d1589ac087b07999e8d92fce8bbecf820d9bd
Giau1020 <ngocgiau2093@gmail.com>
2025-02-05 10:33:37 +0700

    commit homepage

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,8 @@
+function toggleFAQ(element) {
+    var answer = element.nextElementSibling;
+    if (answer.style.display === "none" || answer.style.display === "") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none";
+    }
+}
